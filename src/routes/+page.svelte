127c6ec09cdabd2a42873c0d205b5736dcdbe7b0
Ramon Pocon <pocon.ri@gmail.com>
2023-05-31 23:41:30 -0600

    svelte test

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import HeroesList from './HeroesList.svelte';
+</script>
+
+<svelte:head>
+	<title>Heroes</title>
+	<meta name="description" content="Heroes App" />
+</svelte:head>
+
+<section>
+	<!-- <Counter /> -->
+	<HeroesList />
+</section>
+
+<style>
+	
+</style>
---
+++ src/routes/HeroesList.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+
+let heroes:any[] = [];
+let searchValue = '';
+
+const fetchHeroes = () => {
+    fetch('https://akabab.github.io/superhero-api/api/all.json')
+        .then((res) => res.json())
+        .then((data) => {
+            heroes = data;
+            console.log(heroes,'heroes')
+        });
+};
+fetchHeroes();
+
+$: filteredHeroes = heroes.filter(hero => 
+    hero.name.toLowerCase().includes(searchValue.toLowerCase()) || hero.biography.fullName.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+</script>
+
+<div class="HeroesList">
+    <div
+        class="container"
+    >
+        <h1>Heroes List</h1>
+        <input type='text' bind:value={searchValue} placeholder="Search heroes by name">
+        <div class="heroes-list">
+            {#if searchValue.length > 0 && filteredHeroes.length === 0}
+                <p>No heroes were found with the values provided</p>
+            {:else if filteredHeroes.length > 0}
+            {#each filteredHeroes as hero (hero.id)}
+                <div class="hero-card">
+                <img src={hero.images.sm} alt={hero.name}  class="hero-image"/>
+                <div
+                    class="hero-info"
+                >
+                    <h2 class="hero-name">{hero.name}</h2>
+                    <h2 class="hero-name">{hero.biography.fullName}</h2>
+                </div>
+                
+                </div>
+            {/each}
+        {:else}
+            <p>Loading heroes...</p>
+        {/if}
+    </div>
+    </div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 16px;
+    }
+
+    .heroes-list {
+        display: flex;
+        width: 100%;
+        flex-wrap: wrap;
+        gap: 16px;
+    }
+
+    .hero-card {
+        display: flex;
+        gap: 16px;
+    }
+    
+    .hero-info {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .hero-name {
+        width: 100px;
+        flex-wrap: wrap;
+    }
+
+    .hero-image {
+        width: 100px;
+        height: 100px;
+        object-fit: cover;
+    }
+</style>
